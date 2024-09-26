de4a3e5e3dda5baa19e5009543661c2731275962
Sofia Gamershmidt <gamershmidt.sofya@bk.ru>
2024-09-27 01:05:23 +0300

    base hw

--- code/deployment/app/streamlit_app.py
+++ code/deployment/app/streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import requests
+
+FASTAPI_URL =  "http://fastapi_app:8000/predict"
 
 st.title("Doctor Bot")
 
@@ -12,8 +15,11 @@ for message in st.session_state.messages:
 if prompt := st.chat_input("Enter your sickness symptoms here"):
     st.chat_message("user").markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
+    input_data = {"input_data": prompt}
 
-    response = f"Echo: {prompt}"
+    response = requests.post(FASTAPI_URL, json=input_data)
+    prediction = response.json()
+    response = f"I guess your disease: {prediction['prediction']}"
     with st.chat_message("assistant"):
         st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
